72bc6ba7bd3cbf7b2004b081210fe2e51fe72983
Ítalo Sousa <italusousa@gmail.com>
2017-10-20 10:32:04 -0200

    Laboratório de Programação
    Versão 2.0
    - Retirado o bug
    - Para a entrada 56781 a versão anterior retira o número 1, o numero acaba ficando o 5678, mas não é o menor número. A nova versão compara as combinações possíveis. Retornando assim o número correto 6781, que é a maior possibilidade

--- Quebrando_a_Banca.py
+++ Quebrando_a_Banca.py
@@ -1,15 +1,36 @@
-e1 = input().split()
-e2 = input()
+#v2.0
 
-li = []
-for i in e2:
-	li.append(i)
+#e1 = input().split()
+#e2 = input()
 
+#casos de testes
+e1 = ['6', '1']
+e2 = '313487'
+
+num = e2
 for i in range(int(e1[1])):
-	li.remove(min(li))
-	#esse print mostra o "passo a passo"
-	#print(li)
-char =''
-for i in li:
-	char += i
-print(char)
+	print(i)
+	maior = 0
+	for i in num:
+		#lista vazia pra dps remover o indice
+		li = []
+		for j in num:
+			li.append(j)
+		print(li)
+		
+		#removendo o indice
+		n=''
+		li.remove(i)
+		print(li)
+		
+		#comparação
+		for k in li:
+			n += k
+		print(n)
+		if int(n) > maior:
+			maior = int(n)
+	
+	#salva o valor do primeiro loop		
+	num = str(maior)
+	
+print(maior)
